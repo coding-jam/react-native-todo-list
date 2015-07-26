e403cbff5f25bd3d426c9c449fb7c615a1a8dcff
Francesco Strazzullo <me@francescostrazzullo.info>
2015-07-26 19:20:00 +0200

    Created main App Page

--- index.ios.js
+++ index.ios.js
@@ -1,49 +1,14 @@
 'use strict';
+var App = require('./src/components/App');
 
 var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
 
 var ReactNativeTodoList = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <App></App>
     );
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('ReactNativeTodoList', () => ReactNativeTodoList);
+React.AppRegistry.registerComponent('ReactNativeTodoList', () => ReactNativeTodoList);
---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  Text,
+  View
+} = React;
+
+var styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
+
+module.exports = class extends React.Component{
+	render() {
+	    return (
+	    	<View>
+	    		<Text style={styles.title}>
+          			React Native Todo-List
+        		</Text>
+      		</View>
+	    );
+	}
+}
